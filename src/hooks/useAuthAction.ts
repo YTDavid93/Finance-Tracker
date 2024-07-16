9eba82c72100b29a98d601d0f98e313c5ae0379e
YTDavid93 <ytd3809@gmail.com>
2024-07-16 19:23:31 +0545

    removed unused imports

--- src/hooks/useAuthAction.ts
+++ src/hooks/useAuthAction.ts
@@ -1,7 +1,6 @@
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { toast } from "react-toastify";
-import { ToastError } from "../utils/ToastError";
 import { UserCredential } from "firebase/auth";
 
 interface Props {
