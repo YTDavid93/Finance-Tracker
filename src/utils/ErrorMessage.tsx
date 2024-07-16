0ba26171aa8b3a0735e9caac676781acae8dfe31
YTDavid93 <ytd3809@gmail.com>
2024-07-16 15:55:35 +0545

    build errorMessage component

---
+++ src/utils/ErrorMessage.tsx
@@ -0,0 +1,13 @@
+import { ReactNode } from "react"
+
+interface Props {
+    children: ReactNode;
+}
+
+const ErrorMessage = ({ children }: Props) => {
+  return (
+    <p className="text-red-500 text-sm">{children}</p>
+  )
+}
+
+export default ErrorMessage
