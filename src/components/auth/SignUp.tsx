c01c98dca1430b0e633bcad14b00a8cb4d64fcc3
YTDavid93 <ytd3809@gmail.com>
2024-07-16 19:20:19 +0545

    add form validation and loder

--- src/components/auth/SignUp.tsx
+++ src/components/auth/SignUp.tsx
@@ -4,16 +4,39 @@ import { auth } from "../../Firebase/Firebase";
 import useAuthAction from "../../hooks/useAuthAction";
 import { signWithGooglePopup } from "../../Firebase/googleSignIn";
 import { NavLink } from "react-router-dom";
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+import ErrorMessage from "../../utils/ErrorMessage";
+import { Loader } from "../../utils/Loading";
 
-interface FormData {
-  name: string;
-  email: string;
-  password: string;
-  confirmPassword: string;
-}
+const schema = z
+  .object({
+    name: z
+      .string({ invalid_type_error: "Name is required" })
+      .min(3, { message: "Must be 3 or more characters long" }),
+    email: z
+      .string({ required_error: "Email is required" })
+      .email({ message: "Invalid email address" }),
+    password: z
+      .string({ required_error: "Password is required" })
+      .min(6, { message: "Password must be 6 characters long" }),
+    confirm: z.string(),
+  })
+  .refine((data) => data.password === data.confirm, {
+    message: "Password don't match",
+    path: ["confirm"],
+  });
+
+type FormData = z.infer<typeof schema>;
 
 const SignUp = () => {
-  const { register, handleSubmit } = useForm<FormData>();
+  const {
+    register,
+    handleSubmit,
+    formState: { errors, isValid },
+  } = useForm<FormData>({
+    resolver: zodResolver(schema),
+  });
   const { handleAction, error, loading } = useAuthAction();
 
   const onSubmit: SubmitHandler<FormData> = (data) => {
@@ -52,6 +75,7 @@ const SignUp = () => {
             id="name"
             className="w-full px-3 py-2 rounded-md border focus:outline-none focus:ring focus:border-blue-300"
           />
+          {errors.name && <ErrorMessage>{errors.name.message}</ErrorMessage>}
         </div>
 
         <div className="mb-3">
@@ -62,6 +86,7 @@ const SignUp = () => {
             id="email"
             className="w-full px-3 py-2 rounded-md border focus:outline-none focus:ring focus:border-blue-300"
           />
+          {errors.email && <ErrorMessage>{errors.email.message}</ErrorMessage>}
         </div>
 
         <div className="mb-3">
@@ -72,21 +97,31 @@ const SignUp = () => {
             id="password"
             className="w-full px-3 py-2 rounded-md border focus:outline-none focus:ring focus:border-blue-300"
           />
+          {errors.password && (
+            <ErrorMessage>{errors.password.message}</ErrorMessage>
+          )}
         </div>
 
         <div className="mb-3">
-          <label htmlFor="confirm-password">Confirm Password</label>
+          <label htmlFor="confirm">Confirm Password</label>
           <input
-            {...register("confirmPassword")}
+            {...register("confirm")}
             type="password"
-            id="confirm-password"
+            id="confirm"
             className="w-full px-3 py-2 rounded-md border focus:outline-none focus:ring focus:border-blue-300"
           />
+          {errors.confirm && (
+            <ErrorMessage>{errors.confirm.message}</ErrorMessage>
+          )}
         </div>
 
+        {error && <ErrorMessage>{error}</ErrorMessage>}
+
         <div className="flex flex-col gap-3">
-          <button className="bg-white rounded-lg w-full text-customBlue text-base px-4 py-2 border border-customBlue hover:bg-blue-600 hover:text-white transition-all duration-200 ease-in-out mt-2">
-            {loading ? "Creating Account...." : "Sign Up with Email"}
+          <button className="bg-white rounded-lg w-full text-customBlue text-base px-4 py-2 border border-customBlue hover:bg-blue-600 hover:text-white transition-all duration-200 ease-in-out mt-2"
+            disabled={loading}
+          >
+            {loading ? <Loader /> : "Sign Up with Email"}
           </button>
 
           <h3 className="text-center text-base">or</h3>
@@ -94,12 +129,14 @@ const SignUp = () => {
           <button
             className="bg-blue-500 rounded-lg text-white text-base w-full px-4 py-2 hover:bg-white hover:text-customBlue hover:border hover:border-customBlue transition-all duration-200 ease-in-out"
             onClick={logGoogleUser}
+            disabled={loading}
           >
             Sign Up with google
           </button>
 
           <p className="text-sm text-black text-center">
-           Or Have An Account Already ? <NavLink to="/login">Click Here</NavLink>
+            Or Have An Account Already ?{" "}
+            <NavLink to="/login">Click Here</NavLink>
           </p>
         </div>
       </form>
