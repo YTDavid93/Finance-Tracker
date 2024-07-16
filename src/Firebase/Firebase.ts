b34052ba583e513acb8551fa323e443f2f03ccd6
YTDavid93 <ytd3809@gmail.com>
2024-07-16 13:42:25 +0545

    siginwithemailandpassword config

---
+++ src/Firebase/Firebase.ts
@@ -0,0 +1,23 @@
+import { initializeApp } from "firebase/app";
+import { firebase } from "./apiKeyConfig";
+import { getAuth } from "firebase/auth";
+
+//signin with email and password
+
+export const firebaseConfig = {
+  apiKey: firebase.API_KEY,
+  authDomain: firebase.AUTH_DOMAIN,
+  projectId: firebase.PROJECTID,
+  storageBucket: firebase.STORAGEBUCKET,
+  messagingSenderId: firebase.MESSAGINGID,
+  appId: firebase.APP_ID,
+  measurementId: firebase.MEASUREMENTID,
+};
+
+
+// Initialize Firebase 
+// this one is for signwithemail and password
+const app = initializeApp(firebaseConfig);
+
+export const auth = getAuth(app);
+export default app;
