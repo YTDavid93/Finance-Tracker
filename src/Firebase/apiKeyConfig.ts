55f62ef30efeb5a6e3841b4d919f18734b56bd1c
YTDavid93 <ytd3809@gmail.com>
2024-07-16 13:41:17 +0545

    build firebase api keys config objects.

---
+++ src/Firebase/apiKeyConfig.ts
@@ -0,0 +1,10 @@
+
+export const firebase = {
+  API_KEY: import.meta.env.VITE_API_KEY || "",
+  AUTH_DOMAIN: import.meta.env.VITE_AUTH_DOMAIN || "",
+  PROJECTID: import.meta.env.VITE_PROJECTID || "",
+  STORAGEBUCKET: import.meta.env.VITE_STORAGEBUCKET || "",
+  MESSAGINGID: import.meta.env.VITE_MESSAGINGID || "",
+  APP_ID: import.meta.env.VITE_API_ID || "",
+  MEASUREMENTID: import.meta.env.VITE_MEASUREMENTID || "",
+}; 
