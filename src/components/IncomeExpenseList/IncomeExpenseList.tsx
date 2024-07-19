76e8b7aa06c3cc99e349b54ae5f56bf74cafb307
YTDavid93 <ytd3809@gmail.com>
2024-07-19 13:47:43 +0545

    export interface & changes in styles

--- src/components/IncomeExpenseList/IncomeExpenseList.tsx
+++ src/components/IncomeExpenseList/IncomeExpenseList.tsx
@@ -9,7 +9,7 @@ import {
   TableRow,
 } from "../ui/table";
 
-interface ExpenseIncome {
+export interface ExpenseIncome {
   id: number;
   name: string;
   amount: number;
@@ -23,47 +23,52 @@ interface Props {
 
 const IncomeExpenseList = ({ expensesincomes }: Props) => {
   return (
-    <Table>
-      <TableCaption>My Transactions</TableCaption>
-      <TableHeader>
-        <TableRow>
-          <TableHead className="w-[100px]">Name</TableHead>
-          <TableHead>Amount</TableHead>
-          <TableHead>Date</TableHead>
-          <TableHead>Tag</TableHead>
-          <TableHead>Action</TableHead>
-        </TableRow>
-      </TableHeader>
-      <TableBody>
-        {expensesincomes.length > 0 ? (
-          expensesincomes.map((el) => (
-            <TableRow key={el.id}>
-              <TableCell className="font-medium">{el.name}</TableCell>
-              <TableCell>{el.amount}</TableCell>
-              <TableCell>{el.date}</TableCell>
-              <TableCell>{el.tag}</TableCell>
-              <TableCell>
-                <button>Delete</button>
+    <section className="px-8">
+      <Table className="bg-white rounded-md">
+        {/* <TableCaption>My Transactions</TableCaption> */}
+        <TableHeader>
+          <TableRow>
+            <TableHead>Name</TableHead>
+            <TableHead>Amount</TableHead>
+            <TableHead>Date</TableHead>
+            <TableHead>Tag</TableHead>
+            <TableHead>Action</TableHead>
+          </TableRow>
+        </TableHeader>
+        <TableBody>
+          {expensesincomes.length > 0 ? (
+            expensesincomes.map((el) => (
+              <TableRow key={el.id}>
+                <TableCell className="font-medium">{el.name}</TableCell>
+                <TableCell>{el.amount}</TableCell>
+                <TableCell>{el.date}</TableCell>
+                <TableCell>{el.tag}</TableCell>
+                <TableCell>
+                  <button>Delete</button>
+                </TableCell>
+              </TableRow>
+            ))
+          ) : (
+            <TableRow>
+              <TableCell colSpan={5} className="text-center py-4">
+                No Records Found
               </TableCell>
             </TableRow>
-          ))
-        ) : (
+          )}
+        </TableBody>
+        <TableFooter>
           <TableRow>
-            <TableCell className="text-center">No Records Found</TableCell>
+            <TableCell>Total</TableCell>
+            <TableCell>
+              Rs
+              {expensesincomes
+                .reduce((prevVal, currVal) => currVal.amount + prevVal, 0)
+                .toFixed(2)}
+            </TableCell>
           </TableRow>
-        )}
-      </TableBody>
-      <TableFooter>
-        <TableRow>
-          <TableCell colSpan={3}>Total</TableCell>
-          <TableCell className="text-right">
-            {expensesincomes
-              .reduce((prevVal, currVal) => currVal.amount + prevVal, 0)
-              .toFixed(2)}
-          </TableCell>
-        </TableRow>
-      </TableFooter>
-    </Table>
+        </TableFooter>
+      </Table>
+    </section>
   );
 };
 
