e8fa3ac76bdbd1d6e09e4dbc7dac06086b2a251d
YTDavid93 <ytd3809@gmail.com>
2024-07-19 09:08:21 +0545

    display both income and expense component in dashboard

--- src/components/DashBoard.tsx
+++ src/components/DashBoard.tsx
@@ -1,9 +1,11 @@
+import TotalExpensesForm from "./totalExpenses/TotalExpensesForm";
 import TotalIncomeForm from "./totalIncome/TotalIncomeForm";
 
 const Dashboard = () => {
   return (
     <>
       <TotalIncomeForm onSubmit={(data) => console.log(data)} />
+      <TotalExpensesForm />
     </>
   );
 };
