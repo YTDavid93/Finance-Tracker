7f8b0cd86de8f030f950c3038b4dd1fff7b443b7
YTDavid93 <ytd3809@gmail.com>
2024-07-21 16:16:20 +0545

    changes

--- src/components/search/SearchInput.tsx
+++ src/components/search/SearchInput.tsx
@@ -1,4 +1,3 @@
-import { useRef } from "react";
 import { IoIosSearch } from "react-icons/io";
 
 interface Props {
@@ -6,26 +5,18 @@ interface Props {
 }
 
 const SearchInput = ({ onSearch }: Props) => {
-  const ref = useRef<HTMLInputElement>(null);
-
   return (
-    <form
-      onSubmit={(e) => {
-        e.preventDefault();
-        if (ref.current) onSearch(ref.current.value);
-      }}
-      className="w-full"
-    >
+    <div className="w-full">
       <div className="relative ">
         <IoIosSearch className="absolute bottom-3 left-2" />
         <input
-          ref={ref}
           type="search"
           placeholder="Search by Type..."
           className="w-full px-8 py-2 rounded-md border focus:outline-none focus:ring focus:border-blue-300"
+          onChange={(e) => onSearch(e.target.value)}
         />
       </div>
-    </form>
+    </div>
   );
 };
 
