1ac81f1dc1b49ebf10393e3e5493d54f25c02778
YTDavid93 <ytd3809@gmail.com>
2024-07-25 13:13:50 +0545

    style navbar

--- src/components/Header/NavBar.tsx
+++ src/components/Header/NavBar.tsx
@@ -1,12 +1,20 @@
+import useAuth from "../../hooks/useAuth";
+
 interface Props {
   onClick: () => void;
 }
 
 const NavBar = ({ onClick }: Props) => {
+  const { user } = useAuth();
+
   return (
-    <nav className="bg-[#2970ff] flex justify-evenly">
-      <h1>Financely</h1>
-      <button onClick={onClick}>logout</button>
+    <nav className="bg-[#2970ff] flex justify-between p-6">
+      <h1 className="text-xl font-medium text-white">Financely.</h1>
+
+      <div className="flex items-center gap-4 text-white">
+        <p>User: {user?.displayName}</p>
+        <button onClick={onClick}>logout</button>
+      </div>
     </nav>
   );
 };
